d8f5144fd11624d76ff9a1c2fc62676109c8ae08
andres felipe florian chacon <andresfelipeflorianchacon@iMac-de-andres.local>
2018-07-07 18:45:33 -0500

    Se crea la constante text en la clase App.js y se renderiza

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const text = 'Domotica';
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{text}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
